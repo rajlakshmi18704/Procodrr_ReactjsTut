02516891ac64fd13a0fc7197feda47099ad48f31
rajlakshmi18704 <122472826+rajlakshmi18704@users.noreply.github.com>
2024-05-17 08:30:33 +0530

    Add files via upload

---
+++ Routing/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import Header from './Components/Header'
+
+
+import { Outlet } from 'react-router-dom'
+function App() {
+
+
+  return (
+   <>
+   <Header/>
+<Outlet/>
+
+
+
+   </>
+  )
+}
+
+export default App
